3593174b0d47880d227740db6b9e509fd6714cbb
tanvi-pixer <tanvi.ladva@pixer.digital>
2025-10-04 11:45:37 +0530

    second commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// .eslintrc.js
+module.exports = {
+	// your existing config...
+	rules: {
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/no-unused-vars": "warn",
+		"import/no-anonymous-default-export": "off",
+	},
+};
